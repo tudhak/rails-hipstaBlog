cf7c568fe4d81c08f4a58094394269d7feec6d7e
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-01-03 16:13:26 +0100

    little stimulus controller updates

--- app/javascript/controllers/voting_controller.js
+++ app/javascript/controllers/voting_controller.js
@@ -2,21 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="voting"
 export default class extends Controller {
-  static targets = ["upVote", "downVote", "form", "postRating", "voteValue"];
+  static targets = ["upVote", "downVote", "upForm", "downForm", "postRating", "voteValue"];
 
   connect() {
     console.log(this.updateButtonTarget);
   }
 
-  vote(event) {
+  upVote(event) {
     event.preventDefault();
     let rating = this.postRatingTarget.innerText;
     console.log(Number(rating));
 
-    fetch(this.formTarget.action, {
+    fetch(this.upFormTarget.action, {
       method: "PATCH",
       headers: { "Accept": "text/plain" },
-      body: new FormData(this.formTarget)
+      body: new FormData(this.upFormTarget)
+    })
+      .then(response => response.text())
+      .then((data) => {
+        console.log(data.message_rating)
+        this.postRatingTarget.innerText = Number(this.postRatingTarget.innerText) + Number(this.voteValueTarget.value);
+        this.upVoteTarget.setAttribute("disabled", "true");
+        this.downVoteTarget.setAttribute("disabled", "true");
+      })
+  }
+
+  downVote(event) {
+    event.preventDefault();
+    let rating = this.postRatingTarget.innerText;
+    console.log(Number(rating));
+
+    fetch(this.downFormTarget.action, {
+      method: "PATCH",
+      headers: { "Accept": "text/plain" },
+      body: new FormData(this.downFormTarget)
     })
       .then(response => response.text())
       .then((data) => {
